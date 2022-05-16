faaf2da64ffc81563785d570112242db9a2287d3
Dvir_Levy <dvir.levy@moovit.com>
2022-05-16 17:36:21 +0300

    first commit, before nodmailer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import SignIn from './Components/SignIn';
+import SignUp from './Components/SignUp';
+import ForgotPassword from './Components/ForgotPassword';
+import { Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<SignIn/>} />
+        <Route path="/SignUp" element={<SignUp/>} />
+        <Route path="/ResetPassword" element={<ForgotPassword/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Utils/dal.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+
+const createNewUser = async (obj) =>{
+    console.log("fron createUser\n" + obj)
+    try{
+        await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/signup`,obj)
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+
+const verifyUser = async (obj) =>{
+    console.log("fron verifyUser\n" + obj)
+    try{
+        await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/signin`,obj)
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+
+const resetPass = async (obj) =>{
+    try{
+        return await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/resetpassword`,obj)
+        
+        
+    }
+    catch(error){
+        return error
+    }
+}
+
+
+
+// eslint-disable-next-line
+export default { createNewUser , verifyUser , resetPass } 
